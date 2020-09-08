806bb9a5204284da6ff1d393637a663b51705a48
Mark Madhukar <markmadhukar03@gmail.com>
2020-09-08 15:21:00 +0530

    program file

---
+++ main.py
@@ -0,0 +1,25 @@
+from Student_class import student
+
+student_data = list()
+
+repeat = 'y'
+while repeat.lower() == 'y':
+    data = {}
+    data['name'] = input('/nEnter student\'s name: ')
+    data['s_id'] = input('Enter student ID: ')
+    count = int(input(f'Enter number of subjects chosen by student: '))
+    for i in range(count):
+        subject = input(f'Enter subject name ({i + 1}) : ')
+        data[subject] = int(input(f'Enter {subject} marks: '))
+    repeat = input('do you want continue (y/n): ')
+    
+    student_data.append(student(**data))
+
+for i in student_data:
+    calc_data = i.average()
+    print(f'\nStudent\'s name: {i.name}')
+    for j, k in vars(i).items():
+        if type(k) is int:
+            print(f'{i.name}\'s {j} marks: {k}')
+    print(f'{i.name}\'s total marks: {calc_data[0]}')
+    print(f'{i.name}\'s average marks: {calc_data[1]}')
